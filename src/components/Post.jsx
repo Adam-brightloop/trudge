a9e4fc0f72c4875511936ed1e0400423c4f7c3c1
Adam-brightloop <adam@brightloop.design>
2025-04-16 16:07:58 -0600

    new

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,11 +1,11 @@
-
+import classes from './Post.module.css'; // Import the CSS module for styling
 
 function Post(props) {  
   const { author, body } = props;
   return (
-    <div>
-      <h2>{author}</h2>
-      <p>{body}</p>
+    <div className={classes.post}>
+      <h2 className={classes.author}>{author}</h2>
+      <p className={classes.text}>{body}</p>
     </div>
   );
 }   
