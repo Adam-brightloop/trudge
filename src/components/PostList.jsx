972fd74aad523d0d21cdfc7cddf02ee4e332e6aa
Adam-brightloop <adam@brightloop.design>
2025-04-16 16:56:10 -0600

    onChange event handler in NewPost component

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,12 +1,16 @@
-import Post from './Post'; // Import the Post component
+import Post from './Post';
+import NewPost from './NewPost'; // Import the Post component
 import classes from './PostList.module.css'; // Import the CSS module for styling
 function PostList(){
     return (
-        <ul className={classes.posts}>
+        <>
+            <NewPost />
+            <ul className={classes.posts}>
             <Post author="Adam" body="Master Web Engineer" />
             <Post author="Berta" body="Master Smelly Girl" />
             <Post author="Gracie" body="Little Idiot" />
-        </ul>
+            </ul>
+        </>
         
     );
 }
