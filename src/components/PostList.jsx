473154d00815a247ba445868b0c9e320595e63b9
Adam-brightloop <adam@brightloop.design>
2025-04-16 19:40:25 -0600

    27. Lifting State Up. state for two components

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,13 +1,24 @@
+import{ useState } from 'react'; // Import useState from React
 import Post from './Post';
 import NewPost from './NewPost'; // Import the Post component
 import classes from './PostList.module.css'; // Import the CSS module for styling
 function PostList(){
+    const [enteredBody, setEnteredBody] = useState(''); // Initialize state to hold posts'
+    const [enteredAuthor, setEnteredAuthor] = useState(''); // Initialize state to hold posts' body text
+    function bodyChangeHandler(event) {
+        setEnteredBody(event.target.value); // Update the body variable with the new value from the text area input field.
+    }
+
+    function authorChangeHandler(event) {
+        setEnteredAuthor(event.target.value); // Update the author variable with the new value from the text area input field.
+    }
+
     return (
         <>
-            <NewPost />
+            <NewPost onBodyChange={bodyChangeHandler} onAuthorChange={authorChangeHandler}/>
             <ul className={classes.posts}>
             <Post author="Adam" body="Master Web Engineer" />
-            <Post author="Berta" body="Master Smelly Girl" />
+            <Post author={enteredAuthor} body={enteredBody} />
             <Post author="Gracie" body="Little Idiot" />
             </ul>
         </>
